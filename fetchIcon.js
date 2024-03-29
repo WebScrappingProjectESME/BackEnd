87acabc97b835f9368ee5ed5461543ccd05b3af1
Théau MARGOUTY <theau.margouty@fer.hr>
2024-03-29 18:00:27 +0100

    update

--- fetchIcon.js
+++ fetchIcon.js
@@ -7,7 +7,7 @@ let config = {
   maxBodyLength: Infinity,
   url: 'https://www.steamgriddb.com/api/v2/logos/steam/400',
   headers: {
-    'Authorization': 'Bearer cf90a3b7e93178e59fb8e163ee1e615f'
+    'Authorization': 'Bearer ***'
   }
 };
 
