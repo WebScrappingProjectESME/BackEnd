ecae52e794cb127ddbb5c31e7674283588ca5e44
Théau MARGOUTY <theau.margouty@fer.hr>
2024-04-09 13:25:40 +0200

    last update for true main that create ListOfGames.json

--- trueMain.js
+++ trueMain.js
@@ -36,11 +36,37 @@ const createJson=(T)=>{
     else{
         gameInfo["price"]=-42.69;
     }
+
     gameInfo["screenshot_url"]=[];
+
     const maxSize=T["screenshots"].length<5?T["screenshots"].length:5;
     for (let i = 0; i <maxSize; i++) {
         gameInfo["screenshot_url"].push(T["screenshots"][i]["path_full"]);
     }
+    if(T["dlc"])
+        gameInfo["dlc"]=T["dlc"].length;
+    else{
+        gameInfo["dlc"]=0;
+    }
+
+    gameInfo["tags"]=[];
+    const categoriesList=T["categories"]
+    for (let i = 0; i < categoriesList.length; i++) {
+        const tagid=categoriesList[i]["id"];
+        // 1=Multi 2=Solo 9=coop 49=PvP
+        if(tagid===1 || tagid===2 || tagid===9 || tagid===49){
+            gameInfo["tags"].push(categoriesList[i]["description"]);
+        }
+    }
+    const genresList=T["genres"]
+    for (let i = 0; i < genresList.length; i++) {
+        const tagid=genresList[i]["id"];
+        // 1=Action 2=Stratégie 3=RPG
+        if(tagid===1 || tagid===2 || tagid===3){
+            gameInfo["tags"].push(genresList[i]["description"]);
+        }
+    }
+
     return gameInfo
 }
 const calcScore=(T)=>{
@@ -49,9 +75,90 @@ const calcScore=(T)=>{
     return 0;
 }
 
+const craftDurationGame=()=>{
+    return Math.floor(Math.random()*50+10);
+}
+
+const craftPopHisto=()=>{
+    const Pop={"week":[],"month":[],"year":[]}
+    let calc=0;
+
+    let daily=Math.random()*15000+20000;
+    let weekly=Math.random()*1500+2000;
+    let noise=Math.random()*weekly;
+
+    for (let i = 0; i < 14; i++) {
+         calc=daily*Math.sin(2*i*Math.PI/12)+
+             weekly*Math.sin(2*i*Math.PI/168)+
+             noise*Math.cos(2*i*Math.PI/(1+Math.random()*3))+
+             10000;
+        Pop.week.push(Math.floor(Math.abs(calc)));
+    }
+
+    for (let i = 0; i < 15; i++) {
+        calc=daily*Math.sin(2*i*Math.PI/24)+
+            weekly*Math.sin(2*i*Math.PI/336)+
+            noise*Math.cos(2*i*Math.PI/(2+Math.random()*3))+
+            10000;
+        Pop.month.push(Math.floor(Math.abs(calc)));
+    }
+
+    for (let i = 0; i < 12; i++) {
+        calc=daily*Math.sin(2*i*Math.PI/48)+
+            weekly*Math.sin(2*i*Math.PI/672)+
+            noise*Math.cos(2*i*Math.PI/(2+Math.random()*3))+
+            10000;
+        Pop.year.push(Math.floor(Math.abs(calc)));
+    }
+
+    return Pop;
+}
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+const craftSalesHisto=()=>{
+    const Obj={"Sales":[]};
+    const listPromoStr=["Promo Exceptionnelle",
+                        "Promo coup de coeur",
+                        "Promo fête saisonnière",
+                        "Promo éditeur",
+                        "Promo chaotic",
+                        "Promo fête des jeux"
+    ]
+
+    const max=Math.floor(Math.random()*2+4);
+    const mois=["janvier",
+        "février",
+        "mars",
+        "avril",
+        "mai",
+        "juin",
+        "juillet",
+        "août",
+        "septembre",
+        "octobre",
+        "novembre",
+        "décembre"
+    ]
+    let previousDate=new Date(2020,0,1);
+
+    for (let i = 0; i <max; i++) {
+        let d = randomDate(previousDate, new Date());
+        let temp={}
+        temp["name"]=listPromoStr[Math.floor(Math.random()*6)];
+        temp["date"]=d.getDate().toString()+" "+mois[d.getMonth()].toString()+" "+d.getUTCFullYear();
+        temp["reduc"]=Math.floor(Math.random()*100);
+        previousDate=d;
+        Obj["Sales"].push(temp);
+    }
+
+    return Obj
+}
+
 const getAppById = async (appId) => (await axios.get(`https://store.steampowered.com/api/appdetails?appids=${appId}&cc=fr&l=fr`))//.data["400"].data;
 const getReviewById = async (appId) => (await axios.get(`https://store.steampowered.com/appreviews/${appId}?json=1&language=all`))//.data["query_summary"];
-
+const getCurrentPlayer = async (appId) => (await axios.get(`https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?format=json&appid=${appId}`));
 
 const ListOfGames = {data: []};
 
@@ -61,14 +168,21 @@ for (let i = 0; i < 10; i++) {
     const apiResponse= await getAppById(ListAppId[i])
     //console.log(apiResponse.data[ListAppId[i]])
         if (apiResponse.data[ListAppId[i]]["success"]) {
-            console.log("\n success!!!!!!!!!!!!!!!! \n");
+            console.log("\n Success");
             const gameInfo = createJson(apiResponse.data[ListAppId[i]].data);
             const apiResponseReview=await getReviewById(ListAppId[i]);
+            const apiResponseCurrentPlayer=await  getCurrentPlayer(ListAppId[i]);
             gameInfo["Review"] = calcScore(apiResponseReview.data["query_summary"]);
+            gameInfo["in_game_pop"]= apiResponseCurrentPlayer.data["response"]["player_count"]
+
+            gameInfo["duration"]=craftDurationGame();
+            gameInfo["popHisto"]=craftPopHisto();
+            gameInfo["salesHisto"]=craftSalesHisto()["Sales"];
+
             ListOfGames.data.push(gameInfo);
         }
     else{
-        console.log("\n not in \n");
+        console.log("\n no game");
     }
 }
 const ListOfGamesJson = JSON.stringify(ListOfGames);
