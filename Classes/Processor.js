515f90bb2637f0eb83353e14ea2d1636aa7312b8
Théau MARGOUTY <theau.margouty@fer.hr>
2024-05-14 18:54:11 +0200

    update processor location

---
+++ Classes/Processor.js
@@ -0,0 +1,16 @@
+class Processor{
+
+    constructor(){
+
+    }
+
+    generate = ()=>{
+        return 69
+    }
+
+    procesing =
+        R.when(
+            R.isNil
+            ,generate)
+
+}
