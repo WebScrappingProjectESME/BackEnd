e9e3a1ceea712e575f6d7715a81b786e21e05444
Azok21 <azndark.@gmail.com>
2024-05-14 18:49:31 +0200

    upgrade Collector Class

--- Classes/Collector.js
+++ Classes/Collector.js
@@ -1,67 +1,70 @@
-import axios from "axios"
+import * as R from 'ramda';
+import axios from 'axios';
 
-class Collector {
-  constructor() {
-    this.gameListUrl = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
-    this.appDetailsUrl = 'https://store.steampowered.com/api/appdetails';
-    this.reviewsUrl = 'https://store.steampowered.com/appreviews';
-    this.currentPlayersUrl = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/';
-  }
+export default class Collector {
+  gameListUrl = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
+  appDetailsUrl = 'https://store.steampowered.com/api/appdetails';
+  reviewsUrl = 'https://store.steampowered.com/appreviews';
+  currentPlayersUrl =
+    'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/';
 
-  async getGameList() {
-    try {
-      const response = await axios.get(this.gameListUrl);
-      return response.data.applist.apps;
-    } catch (error) {
+  getGameList = R.tryCatch(
+    async () => (await axios.get(this.gameListUrl)).data.applist.apps,
+    (error) => {
       console.error('Error fetching game list:', error);
       return [];
     }
-  }
+  );
 
-  async getAppById(appId) {
-    try {
-      const response = await axios.get(`${this.appDetailsUrl}?appids=${appId}&cc=fr&l=fr`);
-      return response.data[appId].data; // Assuming response structure is { appId: { data: {} } }
-    } catch (error) {
+  getAppById = R.tryCatch(
+    async (appId) =>
+      (await axios.get(`${this.appDetailsUrl}?appids=${appId}&cc=fr&l=fr`))
+        .data[appId].data,
+    (error, appId) => {
       console.error(`Error fetching app details for appId ${appId}:`, error);
       return null;
     }
-  }
+  );
 
-  async getReviewById(appId) {
-    try {
-      const response = await axios.get(`${this.reviewsUrl}/${appId}?json=1&language=all`);
-      return response.data.query_summary;
-    } catch (error) {
+  getReviewById = R.tryCatch(
+    async (appId) =>
+      (await axios.get(`${this.reviewsUrl}/${appId}?json=1&language=all`)).data
+        .query_summary,
+    (error, appId) => {
       console.error(`Error fetching reviews for appId ${appId}:`, error);
       return null;
     }
-  }
+  );
 
-  async getNumberOfCurrentPlayers(appId) {
-    try {
-      const response = await axios.get(`${this.currentPlayersUrl}?format=json&appid=${appId}`);
-      return response.data.response.player_count;
-    } catch (error) {
-      console.error(`Error fetching current player count for appId ${appId}:`, error);
+  getNumberOfCurrentPlayers = R.tryCatch(
+    async (appId) =>
+      (await axios.get(`${this.currentPlayersUrl}?format=json&appid=${appId}`))
+        .data.response.player_count,
+    (error, appId) => {
+      console.error(
+        `Error fetching current player count for appId ${appId}:`,
+        error
+      );
       return 0;
     }
-  }
+  );
 }
 
-(async () => {
-  const collector = new Collector();
+// TEST ZONE
 
-  const gameList = await collector.getGameList();
-  console.log('Retrieved game list:', gameList);
-
-  const appId = 400; // Example app ID
-  const appDetails = await collector.getAppById(appId);
-  console.log(`Details for app ${appId}:`, appDetails);
-
-  const reviews = await collector.getReviewById(appId);
-  console.log(`Reviews for app ${appId}:`, reviews);
-
-  const playerCount = await collector.getNumberOfCurrentPlayers(appId);
-  console.log(`Current players for app ${appId}:`, playerCount);
-})();
+// (async () => {
+//   const collector = new Collector();
+//
+//   const gameList = await collector.getGameList();
+//   console.log('Retrieved game list:', gameList);
+//
+//   const appId = 400; // Example app ID
+//   const appDetails = await collector.getAppById(appId);
+//   console.log(`Details for app ${appId}:`, appDetails);
+//
+//   const reviews = await collector.getReviewById(appId);
+//   console.log(`Reviews for app ${appId}:`, reviews);
+//
+//   const playerCount = await collector.getNumberOfCurrentPlayers(appId);
+//   console.log(`Current players for app ${appId}:`, playerCount);
+// })();
