af5d2ebcaa19b4392489566ad04f292024c05e63
Azok21 <azndark.@gmail.com>
2024-05-15 16:48:05 +0200

    minor modif on transformer and Processor

--- Classes/Processor.js
+++ Classes/Processor.js
@@ -1,16 +1,32 @@
-class Processor{
+import * as R from 'ramda';
+import {default as Generator} from './Generator.js';
 
-    constructor(){
+export default class Processor {
+  gene = new Generator();
 
-    }
-
-    generate = ()=>{
-        return 69
-    }
-
-    procesing =
-        R.when(
-            R.isNil
-            ,generate)
-
+  controleData = R.map(
+    R.cond([
+      [R.equals('pop'), R.when(R.isEmpty, console.log)(R.prop('pop', gameData))]
+    ])
+  );
 }
+
+// TEST ZONE
+
+const proco = new Processor();
+
+console.log(
+  proco.controleData(['pop', 'reduc', '8'], {
+    pop: '',
+    reduc: 'comment ca va ?',
+    8: 'bien bien'
+  })
+);
+
+console.log(
+  R.prop('pop', {
+    pop: 'Bonsoir',
+    reduc: 'comment ca va ?',
+    8: 'bien bien'
+  })
+);
