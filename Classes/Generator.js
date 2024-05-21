409be872788981e04507bb7bebe8f64cfcc22a95
maxarasta <bdnmaxime@gmail.com>
2024-05-21 19:12:14 +0200

    bite

--- Classes/Generator.js
+++ Classes/Generator.js
@@ -23,10 +23,10 @@ export default class Generator {
   dephasage = 4 / 6 * Math.PI; //Pour que le pique de joueur arrive à 14h
 
   // Equation qui composent notre courbe d'évolution des employés à la semaine
-  weekMF = (x) => this.Player + (this.amplitudeCoef * this.Player) * Math.sin(this.frequency * 2 * Math.PI * x + this.dephasage);
-  weekHF = (x) => this.Player + (this.amplitudeCoef ** 2 * this.Player) * Math.sin(2 * this.frequency * 2 * Math.PI * x + (2 * this.dephasage));
+  weekMF = (x) => (this.amplitudeCoef * this.Player) * Math.sin(this.frequency * 2 * Math.PI * x + this.dephasage);
+  weekHF = (x) => (this.amplitudeCoef ** 2 * this.Player) * Math.sin(2 * this.frequency * 2 * Math.PI * x + (2 * this.dephasage));
 
-  monthMF = 3;
+  monthMF = (x) => (this.amplitudeCoef * this.Player) * Math.sin(this.frequency * 2 * Math.PI * x);
 
   getRandomNumber = () => Math.random() * 2 - 1;
   weekNoise = () => this.noiseCoef * this.getRandomNumber() * this.Player;
@@ -35,7 +35,7 @@ export default class Generator {
   // Création de la liste X des heures à traiter
   listOfHour = R.map(R.multiply(4), R.range(1, 2190));//6 points par jour * 365 jour
 
-  instantPlayer = R.map(R.converge(R.add, [this.weekHF, R.converge(R.add, [this.weekMF, this.weekNoise])]), this.listOfHour); //R.converge pour les mois
+  instantPlayer = R.map(R.reduce(R.add, this.Player, [this.weekHF, this.weekMF, this.weekNoise]), this.listOfHour); //R.converge pour les mois
 
 }
 
@@ -44,7 +44,7 @@ const generator = new Generator();
 const collector = new Collector();
 
 //console.log(`Current players for app ${400}:`, collector.getNumberOfCurrentPlayers(400));
-console.log(`Current players for app ${400}:`, generator.getNumberOfPlayers(400));
+//console.log(`Current players for app ${400}:`, generator.getNumberOfPlayers(400));
 //generator.numberOfPlayers();
-//const valeur = generator.instantPlayer;
-//console.log(valeur);
+const valeur = generator.instantPlayer;
+console.log(valeur);
