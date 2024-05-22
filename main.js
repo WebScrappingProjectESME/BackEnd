1c27f3bd481fd873f00443c43ef76a8da15bef43
Azok21 <azndark.@gmail.com>
2024-05-22 17:01:29 +0200

    progressing on the collect of data for async function

--- main.js
+++ main.js
@@ -9,24 +9,64 @@ const transformer = new Transformer();
 const generator = new Generator();
 
 // Collect and filter appId data
-const appIDList = R.pipeWith(R.andThen, [
-  await collector.getAppIDList,
+const appIDList = await R.pipeWith(R.andThen, [
+  collector.getAppIDList,
   transformer.filterGameName
 ])();
 
-// Collect and format gameData
-const gameDataList = await collector.getAppById(400);
-const gameReviewList = await collector.getReviewById(400);
-const instantPlayerList = await collector.getInstantPlayersById(400);
-
 //console.log(appIDList);
 
-// Filter and format data
+// ################################## HAUT DESSUS CA MARCHE ##########################################################
 
-const formattedGameData = R.pipe(
-  transformer.formatGameData,
-  transformer.GenerateMissingGameData
+// Collect and format gameData
+
+const list = [
+  {appid: 400, name: 'Shadows of Doubt Playtest'},
+  {appid: 2197370, name: 'Reversed Front'},
+  {appid: 2197410, name: 'Celestarium'},
+  {appid: 2197420, name: 'BRoS - Battle Royale of Survival Playtest'}
+];
+
+const promiseAll = (x) => Promise.all(x);
+
+const foo = R.pipe(
+  R.map(
+    R.pipe(
+      R.prop('appid'),
+      R.juxt([
+        //collector.getAppById,
+        //collector.getReviewById
+        collector.getInstantPlayersById
+      ]),
+      promiseAll
+    )
+  ),
+  promiseAll
 );
+console.log(await foo(list));
+
+// console.log(
+//   await R.pipe(
+//     R.juxt([
+//       collector.getAppById,
+//       collector.getReviewById
+//       //collector.getInstantPlayersById
+//     ]),
+//     promiseAll
+//   )(400)
+// );
+
+// ######################## EN DESSOUS ON S'EN FOU C'EST DES TESTE FOIREUX ###########################################
+
+// const gameDataList = await collector.getAppById(400);
+// const gameReviewList = await collector.getReviewById(400);
+// const instantPlayerList = await collector.getInstantPlayersById(400);
+//
+//
+// const formattedGameData = R.pipe(
+//   transformer.formatGameData,
+//   transformer.GenerateMissingGameData
+// );
 
 //const gameDataList = R.map(console.log)(filteredAppIDList);
 
