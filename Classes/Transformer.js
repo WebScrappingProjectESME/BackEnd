9b93d13d8738f952f7c363d2f11e57dc7ec48647
Azok21 <azndark.@gmail.com>
2024-05-22 23:20:47 +0200

    continuity of the construiction of the main and processing to fez modifications in that process

--- Classes/Transformer.js
+++ Classes/Transformer.js
@@ -4,59 +4,79 @@ import {default as Generator} from './Generator.js';
 
 export default class Transformer {
   generator = new Generator();
-  whiteList = [
-    'name',
-    'price_overview',
-    'screenshots',
-    'dlc',
-    'categories',
-    'genres'
-  ];
+  whiteList = ['price_overview', 'screenshots', 'dlc', 'categories', 'genres'];
 
+  // Filter appIdData Function
   filterGameName = R.filter(
     R.pipe(
       R.prop('name'),
       R.and(R.test(/^[\x00-\x7F]*$/), R.pipe(R.equals(''), R.not))
     )
   );
+
+  // Basic Transformation Function
   filterGameData = R.pick(this.whiteList);
+  formatGameData = R.applySpec({key: R.prop(0), data: R.prop(1)});
 
-  formatFilteredGameData = R.pipe(
-    this.filterGameData,
-    R.toPairs,
-    R.map(R.applySpec({key: R.prop(0), data: R.prop(1)}))
-  );
-
-  formatGameData = R.applySpec({
-    name: R.prop('name'),
-    data: this.formatFilteredGameData
-  });
-
+  // Generate and Format Missing Data Functions
   formatGeneratedData = R.cond([
     [
       R.equals('population'),
-      () =>
-        R.fromPairs([
-          ['key', 'population'],
-          ['data', this.generator.instantPlayer]
-        ])
+      () => this.formatGameData(['population', this.generator.instantPlayer])
     ],
-    [
-      R.equals('salesHistory'),
-      () =>
-        R.fromPairs([
-          ['key', 'salesHistory'],
-          ['data', []]
-        ])
-    ]
+    [R.equals('salesHistory'), () => this.formatGameData(['salesHistory', []])]
   ]);
 
-  GenerateMissingGameData = R.pipe(
-    R.prop('data'),
+  generateMissingGameData = R.pipe(
     R.append(this.formatGeneratedData('population')),
     R.append(this.formatGeneratedData('salesHistory'))
   );
 
+  // Filter and Format GameData Functions
+
+  filterAndFormatGameData = R.pipe(
+    this.filterGameData,
+    R.toPairs,
+    R.map(this.formatGameData)
+  );
+
+  controlGameData = R.pipe(
+    this.filterAndFormatGameData,
+    this.generateMissingGameData
+  );
+
+  transformGameData = R.applySpec({
+    name: R.prop('name'),
+    data: this.controlGameData
+  });
+
+  formatInstantPopulationData = (instantPop) =>
+    R.fromPairs([
+      ['key', 'instantPopulation'],
+      ['data', instantPop]
+    ]);
+
+  fuseGameData = (gameData) =>
+    R.append(R.prop(0, gameData), R.prop(1, gameData));
+
+  // Format and calculate price and review
+
+  formatGameReview = (reviewData) =>
+    R.fromPairs([
+      ['key', 'review'],
+      [
+        'data',
+        R.multiply(
+          100,
+          R.divide(
+            R.prop('total_positive', reviewData),
+            R.prop('total_reviews', reviewData)
+          ).toFixed(4)
+        )
+      ]
+    ]);
+
+  // Saving function
   saveAsFile(path, object) {
     try {
       fs.writeFileSync(path, JSON.stringify(object));
@@ -64,18 +84,19 @@ export default class Transformer {
       console.error('Error writing JSON data to file:', error);
     }
   }
-
-  // For Optimisation if needed ##########################################
-
-  formatGameID = R.pipe(R.map(R.props(['name', 'appId'])), R.fromPairs);
-  splitGameData(object) {}
-  fuseGameData(object) {}
-
-  // #####################################################################
 }
 
 // TEST ZONE
 
-// const trans = new Transformer();
+const trans = new Transformer();
 //
-// console.log(trans.formatFilteredGameData({type: 'game', pop: []}));
+// const priceData = {
+//   num_reviews: 20,
+//   review_score: 9,
+//   review_score_desc: 'Overwhelmingly Positive',
+//   total_positive: 132181,
+//   total_negative: 2008,
+//   total_reviews: 134189
+// };
+//
+// console.log(trans.formatGameReview(priceData));
