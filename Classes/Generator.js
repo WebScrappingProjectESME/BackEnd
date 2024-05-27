fe4dbf3209f9c7a6d07657c5b30eb4e24952c9c5
LouisMucha <louis.mc59@gmail.com>
2024-05-28 00:33:19 +0200

    generator refactored

--- Classes/Generator.js
+++ Classes/Generator.js
@@ -1,88 +1,32 @@
 import * as R from "ramda";
 import {default as Collector} from './Collector.js';
-import axios from "axios";
 
 export default class Generator {
     collector = new Collector();
 
-    // async playerCount(appId) {return collector.getNumberOfCurrentplayers(appId);};
-
-    ///// List of t-time player
-    playerCount = R.tryCatch(
-        async (appId) => {
-            return this.collector.getInstantPlayersById(appId);
-        },
-        (error) => {
-            console.error('Error fetching number of current players:', error);
-            return [];
-        }
-    );
-
-    player = 10000;
-    noiseCoef = 0.1; //10% des joueurs
+    // Création des données
+    noiseCoeff = 0.1; //10% des joueurs pour le bruit au maximum
     frequency = 1 / 24; //1 fois toutes les 24 heures
-    amplitudeCoef = 0.6;
+    amplitudeCoeff = 0.6;
     dephasage = 4 / 6 * Math.PI; //Pour que le pique de joueur arrive environ à 14h
 
-    // Equation qui composent notre courbe d'évolution des employés à la semaine
-    weekLF = (player) => (x) => (this.amplitudeCoef * player) * Math.sin(this.frequency * 2 * Math.PI * x + this.dephasage); //product
-    weekHF = (player) => (x) => (this.amplitudeCoef ** 2 * player) * Math.sin(2 * this.frequency * 2 * Math.PI * x + (2 * this.dephasage));
-    monthF = (player) => (x) => (this.amplitudeCoef * player) * Math.sin(this.frequency * 2 * Math.PI * x);
-    yearsF = () => 0;
-    weekNoise = (player) => () => {
+    // Equation qui composent notre courbe d'évolution des employés
+    weekLowFreq = (player) => (x) => R.product([this.amplitudeCoeff,player,Math.sin(R.add(R.product([this.frequency,2,Math.PI,x]),this.dephasage))]); //product
+    weekHighFreq = (player) => (x) => R.product([this.amplitudeCoeff,this.amplitudeCoeff,player,Math.sin(R.add(R.product([2,this.frequency,2,Math.PI,x]),R.product([2,this.dephasage])))]);
+    monthFreq = (player) => (x) => R.product([this.amplitudeCoeff,player,Math.sin(R.product([this.frequency,2,Math.PI,x]))]);
+    yearsFreq = () => 0;
+    getRandomNoise = (player) => () => {
         const getRandomNumber = Math.random() * 2 - 1;
-        return this.noiseCoef * player * getRandomNumber;
+        return this.noiseCoeff * player * getRandomNumber;
     };
 
-    curriedWeekLF = R.curry(this.weekLF);
-    curriedWeekHF = R.curry(this.weekHF);
-    curriedMonthF = R.curry(this.monthF);
-
-
-    sumFunctionOfGame = async (numOfGame) => {
-        try {
-            const player = await this.collector.getInstantPlayersById(numOfGame);
-            const weekLFOfGame = this.curriedWeekLF(player, R.__);
-            const weekHFOfGame = this.curriedWeekHF(player, R.__);
-            const monthFOfGame = this.curriedMonthF(player, R.__);
-            const noiseOfGame  = this.weekNoise(player);
-            const yearsFOfGame = this.yearsF;
-
-            return (x) => player + weekLFOfGame(x) + weekHFOfGame(x) + monthFOfGame(x) + noiseOfGame + yearsFOfGame;
-
-        } catch (error) {
-            console.error('Error in sumFunctionOfGame:', error);
-            return NaN;
-        }
-    };
-
-    listOfHour = R.map(R.multiply(4), R.range(1, 2190));//6 points par jour * 365 jour
-    givePlayerCountForGame = R.pipeWith(R.andThen,[this.collector.getInstantPlayersById,R.juxt([])])
-    //generateForX =  R.map(R.pipe(R.juxt(this.sumFunctionOfGame)))
-
-
-
-    generate = async (AppId) => {
+    generateListOfPlayers = async (AppId) => {
         const listOfHour = R.map(R.multiply(4), R.range(1, 2190));
-        const currentPLayer = await this.collector.getInstantPlayersById(AppId);
-        return listOfHour.map(R.pipe(R.juxt([this.curriedWeekLF(currentPLayer), this.curriedWeekHF(currentPLayer), this.curriedMonthF(currentPLayer), this.weekNoise(currentPLayer)]), R.sum, R.add(currentPLayer), Math.floor));
+        const currentPlayer = await this.collector.getInstantPlayersById(AppId);
+        return listOfHour.map(R.pipe(R.juxt([this.weekLowFreq(currentPlayer), this.weekHighFreq(currentPlayer), this.monthFreq(currentPlayer), this.getRandomNoise(currentPlayer)]), R.sum, R.add(currentPlayer), R.add(this.yearsFreq()), Math.floor));
     }
 
-    /*
-    // Création de la liste X des heures à traiter
-
-
-    transformedPlayerCount = {
-        R.map(
-            R.pipe(
-                R.juxt([x => 10000, this.weekLFOfGame, this.weekHFOfGame, this.monthFOfGame]),
-                R.sum)
-        );
-    }*/
-
-
-
-    ////// Date
+    // Création des réductions
     getRandomDate() {
         const startDate = new Date('2018-01-01');
         const endDate = new Date();
@@ -112,7 +56,7 @@ export default class Generator {
         R.sum
     );
 
-    getOneObjectSale = () => {
+    getOneRandomObjectSale = () => {
         const listPromoStr = [
             'special event sales',
             'random sales',
@@ -131,10 +75,9 @@ export default class Generator {
         return { Title: randomTitle, Date: randomDate, Sale: randomSale };
     }
 
-    // Fonction pour obtenir une liste de réduction
     getRandomListOfSale() {
         const randomNumberOfSale = this.getRandomNumberOfSale();
-        const listOfSales = R.times(this.getOneObjectSale, randomNumberOfSale);
+        const listOfSales = R.times(this.getOneRandomObjectSale, randomNumberOfSale);
 
         const compareResult = R.pipe(R.prop("Date"), this.resultToNumber);
         const sortByResult = R.sortBy(compareResult);
@@ -146,8 +89,5 @@ export default class Generator {
 //// TEST DEBUG
 const generator = new Generator();
 
-//console.log(`Current players for app ${400}:`, await collector.getInstantPlayersById(400));
-//console.log(`Current players for app ${400} :`, await generator.playerCount(400));
-//console.log("List of transformed players count", generator.transformedPlayerCount(generator.listOfHour));
-
-console.log(await  generator.generate(400));
+//console.log(await  generator.generateListOfPlayers(400));
+//console.log(generator.getRandomListOfSale())
