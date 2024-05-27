27c78714e03628f82a8788e90ca49d8b0aa3d56f
Azok21 <azndark.@gmail.com>
2024-05-28 01:22:53 +0200

    adding modification for the price and dlc data and modifying the name filter a bit

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ import {default as Transformer} from './Classes/Transformer.js';
 const collector = new Collector();
 const transformer = new Transformer();
 
+// Set the WhiteList
+transformer.setWhiteList = [
+  'price_overview',
+  'screenshots',
+  'dlc',
+  'categories',
+  'genres'
+];
+
 // Collect and filter appId data
 const appIDList = await R.pipeWith(R.andThen, [
   collector.getAppIDList,
@@ -16,28 +25,27 @@ const appIDList = await R.pipeWith(R.andThen, [
 // Collect and format gameData
 const promiseAll = (x) => Promise.all(x);
 
-const getFormattedGameData = R.pipeWith(R.andThen(), [
+const getAndFormatGameData = R.pipeWith(R.andThen(), [
   collector.getAppById,
   transformer.transformGameData
 ]);
 
-console.log(await getFormattedGameData(400));
+// to do : => instant pop to add,
+//         => add duration
+//         => finish review
 
-const getInstantPopulationData = R.pipeWith(R.andThen, [
-  collector.getInstantPlayersById,
-  transformer.formatInstantPopulationData
-]);
+console.log(await getAndFormatGameData(400));
 
-const list = [
-  {appid: 400, name: 'Portal'},
-  {appid: 2197370, name: 'Reversed Front'},
-  {appid: 2197410, name: 'Celestarium'},
-  {appid: 2197420, name: 'BRoS - Battle Royale of Survival Playtest'}
-];
+// const list = [
+//   {appid: 400, name: 'Portal'},
+//   {appid: 2197370, name: 'Reversed Front'},
+//   {appid: 2197410, name: 'Celestarium'},
+//   {appid: 2197420, name: 'BRoS - Battle Royale of Survival Playtest'}
+// ];
 
-const gameDataList = await R.pipe(
-  R.map(R.pipe(R.prop('appid'), await getFormattedGameData)),
-  promiseAll
-)(list);
+// const gameDataList = await R.pipe(
+//   R.map(R.pipe(R.prop('appid'), await getAndFormatGameData)),
+//   promiseAll
+// )(list);
 
 // console.log(gameDataList);
